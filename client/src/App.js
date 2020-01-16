5edee1c8d704cecfcbcd21c86e6247918da54798
fuston05 <fuston05@yahoo.com>
2020-01-15 21:21:04 -0500

    got part 2 working finally

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
    const addToSavedList = movie => {
       setSavedList([...savedList, movie]);
    };
-
+// huh
    return (
       <div>
          <SavedList list={savedList} />
